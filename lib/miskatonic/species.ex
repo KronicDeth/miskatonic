864a3b95075a32279e2b42dcd90ffcda335d394e
Luke Imhoff <Kronic.Deth@gmail.com>
2017-11-19 16:39:07 -0600

    client_start_link callback

--- lib/miskatonic/species.ex
+++ lib/miskatonic/species.ex
@@ -1,6 +1,11 @@
 defmodule Miskatonic.Species do
+  @callback client_start_link() ::
+              {:ok, pid} | {:error, reason :: term}
+
   defmacro __using__([]) do
     quote do
+      @behaviour Miskatonic.Species
+
       def get(id) do
         with {:ok, client_pid} <- client_start_link() do
           Miskatonic.Client.show(client_pid, id)
