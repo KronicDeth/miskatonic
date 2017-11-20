6fb6210459974694d533d9cc537018d4ed6430e7
Luke Imhoff <Kronic.Deth@gmail.com>
2017-11-19 19:52:12 -0600

    Use Coleopterous Yithian if Library can't find the Yithian

--- lib/miskatonic/yithians.ex
+++ lib/miskatonic/yithians.ex
@@ -1,9 +1,18 @@
 defmodule Miskatonic.Yithians do
   use Miskatonic.Species
 
-  def client_start_link do
-    Miskatonic.Clients.Psychic.start_link(
-      yithian: "Librarian"
-    )
+  def client_start_link(keywords \\ [yithian: "Librarian"]) do
+    Miskatonic.Clients.Psychic.start_link(keywords)
+  end
+
+  def get(id) do
+    case Miskatonic.Species.get(__MODULE__, id) do
+      {:error, :not_found} ->
+        with {:ok, pid} <- client_start_link(yithian: "Coleopterous") do
+          Miskatonic.Client.show(pid, id)
+        end
+      found ->
+        found
+    end
   end
 end
