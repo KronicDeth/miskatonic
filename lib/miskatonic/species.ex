30c5d38022873eba1cb4a4bd5b303b1459ed7403
Luke Imhoff <Kronic.Deth@gmail.com>
2017-11-19 19:25:23 -0600

    Move Miskatonic.Species.get out of quote

--- lib/miskatonic/species.ex
+++ lib/miskatonic/species.ex
@@ -6,11 +6,13 @@ defmodule Miskatonic.Species do
     quote do
       @behaviour Miskatonic.Species
 
-      def get(id) do
-        with {:ok, client_pid} <- client_start_link() do
-          Miskatonic.Client.show(client_pid, id)
-        end
-      end
+      def get(id), do: Miskatonic.Species.get(__MODULE__, id)
+    end
+  end
+
+  def get(module, id) do
+    with {:ok, client_pid} <- module.client_start_link() do
+      Miskatonic.Client.show(client_pid, id)
     end
   end
 end
